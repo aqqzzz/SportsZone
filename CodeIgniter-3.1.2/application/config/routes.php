323079ba167bbd1968b7883b8f9b01be7818618a
aqqzzz <592710057@qq.com>
2016-11-09 21:04:59 +0800

    11.9learn

--- CodeIgniter-3.1.2/application/config/routes.php
+++ CodeIgniter-3.1.2/application/config/routes.php
@@ -49,7 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Page/view';
+$route['news/create'] = 'news/create';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+
 $route['(:any)'] = 'Page/view/$1';
+$route['default_controller'] = 'Page/view';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+//$route['translate_uri_dashes'] = FALSE;
---
+++ CodeIgniter-3.1.2/application/views/news/view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: st0001
+ * Date: 2016/11/9 0009
+ * Time: 11:19
+ */
+echo '<h2>'.$news_item['title'].'</h2>';
+echo $news_item['text'];
