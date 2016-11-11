ac5781b73704400549530d62ff60d427deb7eaf9
aqqzzz <592710057@qq.com>
2016-11-11 15:34:15 +0800

    commit

--- CodeIgniter-3.1.2/application/views/news/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: st0001
- * Date: 2016/11/9 0009
- * Time: 11:19
- */
-echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
--- public_html/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config/smarty_init.php";
-require_once "./common/common.php";
-
-$error = get_error();
-$tpl->assign("error", $error);
-
-$tpl->display("register.html");
-?>
